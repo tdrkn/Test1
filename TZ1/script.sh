e5773fcc68daebc16f9a697e19fab96c5d0f6025
arxungo <danil.rastypain@gmail.com>
2024-05-03 01:23:07 +0300

    add

--- TZ1/script.sh
+++ TZ1/script.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Объявление входящей и исходящей папки
 in_dir="$1"
 out_dir="$2"
 
+#Поиск файлов в каталоге
 find "$in_dir" -type f | while IFS= read -r f; do
     fn=$(basename -- "$f")
     tgt="$out_dir/$fn"
 
+    #Проверка на файлы с одинаковым названием
     cnt=0
     while [[ -e "$tgt" ]]; do
         ext="${fn##*.}"
